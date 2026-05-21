feb9697c75cd9a26c22ff200ac731e70ac1b5110
tomyroko <tomyroko@gmail.com>
2026-05-21 20:15:36 +0200

    Creamos el componente Navbar y lo agregamos a App.jsx

--- frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/App.jsx
+++ frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/App.jsx
@@ -1,18 +1,16 @@
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-
 import "./App.css";
-
 
 function App() {
   return (
     <>
       <Header />
-      <nav></nav>
+      <Navbar />
 
-      <main>
-      </main>
+      <main></main>
 
       <Footer />
     </>
