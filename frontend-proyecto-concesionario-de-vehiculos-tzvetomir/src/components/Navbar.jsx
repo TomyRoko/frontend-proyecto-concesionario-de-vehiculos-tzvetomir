faab7c91f4ccc6d5b16d2a7bea4ef48fab4829c7
tomyroko <tomyroko@gmail.com>
2026-05-21 20:45:58 +0200

    Creamos los enlaces de Navbar

--- frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/components/Navbar.jsx
+++ frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 function Navbar() {
   return (
     <nav>
-      {}
+      <a href="/">Inicio</a>
+      <a href="/Berlina">Berlina</a>
+      <a href="/Coupe">Coupe</a>
+      <a href="/Suv">Suv</a>
+      <a href="/Deportivo">Deportivo</a>
+      <a href="/Electrico">Eléctrico</a>
     </nav>
   );
 }
