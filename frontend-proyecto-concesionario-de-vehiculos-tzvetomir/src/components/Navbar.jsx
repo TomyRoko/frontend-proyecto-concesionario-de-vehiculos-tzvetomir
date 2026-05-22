8973b39c9fa761e32848726bbc7a4fce094f7bed
tomyroko <tomyroko@gmail.com>
2026-05-22 20:51:32 +0200

    terminamos de centrar el header

--- frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/components/Navbar.jsx
+++ frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav>
+    <nav className="header-position" style={{ display: "flex", gap: "20px" }}>
       <a href="/">Inicio</a>
       <a href="/Berlina">Berlina</a>
       <a href="/Coupe">Coupe</a>
--- frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/components/header.jsx
+++ frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/components/header.jsx
@@ -4,8 +4,11 @@ function Header() {
   return (
     <header className="site-header">
       <div className="header-content container">
-        <div className="logo"><img src="./img/logo.png" alt="Logo de la marca" /></div>
+        <div className="logo header-position">
+          <img src="./img/logo.png" alt="Logo de la marca" />
+        </div>
         <Navbar />
+        <span className="header-position"></span>
       </div>
     </header>
   );
