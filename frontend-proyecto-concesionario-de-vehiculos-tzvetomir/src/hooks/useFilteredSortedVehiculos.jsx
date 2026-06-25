a2dbf3dc78a27ead67c65d85569e28b858efb153
tomyroko <tomyroko@gmail.com>
2026-06-25 19:17:34 +0200

    creamos otro componente mas para otro filtro de busqueda del apartado del catalogo

---
+++ frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/hooks/useFilteredSortedVehiculos.jsx
@@ -0,0 +1,33 @@
+function useFilteredSortedVehiculos(vehiculos, search, combustibleFilter, sortBy) {
+  const filteredVehicles = vehiculos.filter((vehicle) => {
+    const matchSearch =
+      vehicle.marca.toLowerCase().includes(search.toLowerCase()) ||
+      vehicle?.modelo?.toLowerCase().includes(search.toLowerCase());
+    const matchCombustible = combustibleFilter
+      ? vehicle.combustible === combustibleFilter
+      : true;
+    return matchSearch && matchCombustible;
+  });
+  const sortedVehicles = [...filteredVehicles].sort((a, b) => {
+    if (sortBy === "az") {
+      if (a.marca < b.marca) return -1;
+      if (a.marca > b.marca) return 1;
+      return 0;
+    }
+    if (sortBy === "za") {
+      if (a.marca > b.marca) return -1;
+      if (a.marca < b.marca) return 1;
+      return 0;
+    }
+    if (sortBy === "newest") {
+      return b.anio - a.anio;
+    }
+
+    if (sortBy === "oldest") {
+      return a.anio - b.anio;
+    }
+  });
+  return { filteredVehicles, sortedVehicles };
+}
+
+export default useFilteredSortedVehiculos;
--- frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/pages/Catologo.jsx
+++ frontend-proyecto-concesionario-de-vehiculos-tzvetomir/src/pages/Catologo.jsx
@@ -3,6 +3,7 @@ import vehiculos from "../data/vehiculos.js";
 import "../index.css";
 import VehiculosList from "../components/VehiculosList.jsx";
 import VehiculoFilters from "../components/VehiculoFilters.jsx";
+import useFilteredSortedVehiculos from "../hooks/useFilteredSortedVehiculos.jsx";
 
 
 function Catalogo() {
@@ -10,35 +11,12 @@ function Catalogo() {
   const [combustibleFilter, setCombustibleFilter] = useState("");
   const [sortBy, setSortBy] = useState("");
 
-  const filteredVehicles = vehiculos.filter((vehicle) => {
-    const matchSearch =
-      vehicle.marca.toLowerCase().includes(search.toLowerCase()) ||
-      vehicle?.modelo?.toLowerCase().includes(search.toLowerCase());
-    const matchCombustible = combustibleFilter
-      ? vehicle.combustible === combustibleFilter
-      : true;
-    return matchSearch && matchCombustible;
-  });
-
-  const sortedVehicles = [...filteredVehicles].sort((a, b) => {
-    if (sortBy === "az") {
-      if (a.marca < b.marca) return -1;
-      if (a.marca > b.marca) return 1;
-      return 0;
-    }
-    if (sortBy === "za") {
-      if (a.marca > b.marca) return -1;
-      if (a.marca < b.marca) return 1;
-      return 0;
-    }
-    if (sortBy === "newest") {
-      return b.anio - a.anio;
-    }
-
-    if (sortBy === "oldest") {
-      return a.anio - b.anio;
-    }
-  });
+  const { filteredVehicles, sortedVehicles } = useFilteredSortedVehiculos(
+    vehiculos,
+    search,
+    combustibleFilter,
+    sortBy
+  );
 
   const hasResults = filteredVehicles.length > 0;
 
